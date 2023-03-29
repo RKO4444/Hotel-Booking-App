dbad517997b7714685de6ab7656023117376eb48
vivek singh <viveksingh9000rk@gmail.com>
2023-03-29 23:10:48 +0530

    inital commitr

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return view('welcome amit  after vivek');
+    return view('welcome amit  after vivek singh');
     
 });
